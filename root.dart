3a959ac2e907023a885e8c8bb912b5ca35b2b190
shinya-07 <72593298+shinya-07@users.noreply.github.com>
2021-09-11 17:57:06 +0900

    Add files via upload

---
+++ root.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+//各ページ読み込み
+import 'routes/home_route.dart';
+import 'routes/send_route.dart';
+import 'routes/setting_route.dart';
+
+class RootWidget extends StatefulWidget {
+  RootWidget({Key? key}) : super(key: key);
+
+  @override
+  _RootWidgetState createState() => _RootWidgetState();
+}
+
+class _RootWidgetState extends State<RootWidget> {
+  int _selectedIndex = 0;
+  final _bottomNavigationBarItems = <BottomNavigationBarItem>[];
+
+  // アイコン情報
+  static const _footerIcons = [
+    Icons.home,
+    Icons.settings,
+    Icons.send,
+  ];
+
+  // アイコン文字列
+  static const _footerItemNames = [
+    'ホーム',
+    '打検設定',
+    'クラウド送信',
+  ];
+
+  var _routes = [
+    Home(),
+    Setting(),
+    Send(),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _bottomNavigationBarItems.add(_UpdateActiveState(0));
+    for (var i = 1; i < _footerItemNames.length; i++) {
+      _bottomNavigationBarItems.add(_UpdateDeactiveState(i));
+    }
+  }
+
+  /// インデックスのアイテムをアクティベートする
+  BottomNavigationBarItem _UpdateActiveState(int index) {
+    return BottomNavigationBarItem(
+        icon: Icon(
+          _footerIcons[index],
+          color: Colors.black87,
+        ),
+        title: Text(
+          _footerItemNames[index],
+          style: TextStyle(
+            color: Colors.black87,
+          ),
+        ));
+  }
+
+  /// インデックスのアイテムをディアクティベートする
+  BottomNavigationBarItem _UpdateDeactiveState(int index) {
+    return BottomNavigationBarItem(
+        icon: Icon(
+          _footerIcons[index],
+          color: Colors.black26,
+        ),
+        title: Text(
+          _footerItemNames[index],
+          style: TextStyle(
+            color: Colors.black26,
+          ),
+        ));
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _bottomNavigationBarItems[_selectedIndex] =
+          _UpdateDeactiveState(_selectedIndex);
+      _bottomNavigationBarItems[index] = _UpdateActiveState(index);
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Scaffold Widget を返す
+    return Scaffold(
+      // ボディ画面は、選択されたアイコンによって変更
+      body: _routes.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed, // これを書かないと3つまでしか表示されない
+        items: _bottomNavigationBarItems,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
